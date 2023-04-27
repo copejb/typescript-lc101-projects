4b0af7d651ec05856c6f97be808764b311dc4600
Jade Cope <jade@Jades-MacBook-Pro.local>
2023-04-26 19:52:59 -0500

    submit studio

---
+++ studio/Rocket.js
@@ -0,0 +1,50 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Rocket = void 0;
+;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sum = 0;
+        for (var i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+        return sum;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.addAstronaut(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
